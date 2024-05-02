42c770a3a1a5c2bb01de280547ef2d0cf59411a1
LUF-FY <75016792+LUF-FY@users.noreply.github.com>
2024-05-02 21:00:47 +0300

    Поправил имена

--- Assets/AmirFolder/ScriptAmir/Error.cs
+++ Assets/AmirFolder/ScriptAmir/Error.cs
@@ -4,37 +4,38 @@ using UnityEngine;
 
 public class Error : MonoBehaviour
 {
-public GameObject error;
-public GameObject triggerError;
+    public GameObject error;
+    public GameObject triggerError;
 
 
-public void Start()
-{
-    error.active = false;
-    Screen.fullScreen = true;    
-}
-public void Could()
- {
-  Cursor.lockState = CursorLockMode.Confined;
-  Cursor.visible = false;      
-  Destroy(triggerError);  
-  Destroy(error);
-  GetComponent<Player>().enabled = true;
-  GetComponent<Camera>().enabled = true;
-  GetComponentInChildren<aperator>().enabled = true;
- }
-private void OnTriggerEnter(Collider other)
+    public void Start()
     {
-       if(this.CompareTag("Error") && other.CompareTag("Player"))
-       {        
-        Screen.fullScreen = false;
+        error.active = false;
+        Screen.fullScreen = true;
+    }
+    public void Could()
+    {
         Cursor.lockState = CursorLockMode.Confined;
-        Cursor.visible = true;      
-        error.active = true;
-        GetComponent<Player>().enabled = false;
-        GetComponent<Camera>().enabled = false;
-        GetComponentInChildren<aperator>().enabled = false;                  
-       }
+        Cursor.visible = false;
+        Destroy(triggerError);
+        Destroy(error);
+        GetComponent<Player>().enabled = true;
+        GetComponent<Camera>().enabled = true;
+        GetComponentInChildren<LogicManager>().enabled = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (this.CompareTag("Error") && other.CompareTag("Player"))
+        {
+            Screen.fullScreen = false;
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+            error.active = true;
+            GetComponent<Player>().enabled = false;
+            GetComponent<Camera>().enabled = false;
+            GetComponentInChildren<LogicManager>().enabled = false;
+        }
     }
 }
 
--- Assets/AmirFolder/ScriptAmir/Player.cs
+++ Assets/AmirFolder/ScriptAmir/Player.cs
@@ -10,73 +10,75 @@ public class Player : MonoBehaviour
     public float speed;
     public GameObject comp;
     public GameObject can;
-    public Image load;    
+    public Image load;
     public GameObject Trigger;
 
     private Vector3 _moveVector;
-    private float _fallVelocity=0;
+    private float _fallVelocity = 0;
 
     private CharacterController _characterController;
     // Start is called before the first frame update
     void Start()
     {
-     _characterController = GetComponent<CharacterController>();
-     load.fillAmount = 0;   
+        _characterController = GetComponent<CharacterController>();
+        load.fillAmount = 0;
     }
 
     void Update()
     {
         _moveVector = Vector3.zero;
-     Uprav();   
+        Uprav();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-       _characterController.Move(_moveVector * speed * Time.fixedDeltaTime);
-       _fallVelocity += gravity * Time.fixedDeltaTime;
-       _characterController.Move(Vector3.down*_fallVelocity * Time.fixedDeltaTime);
-       if(_characterController.isGrounded)
-       {
-         _fallVelocity = 0;
-       }      
+        _characterController.Move(_moveVector * speed * Time.fixedDeltaTime);
+        _fallVelocity += gravity * Time.fixedDeltaTime;
+        _characterController.Move(Vector3.down * _fallVelocity * Time.fixedDeltaTime);
+        if (_characterController.isGrounded)
+        {
+            _fallVelocity = 0;
+        }
     }
+
     void Uprav()
     {
-     if(Input.GetKeyDown(KeyCode.Space) && _characterController.isGrounded)
-            _fallVelocity =-jumpForce;
-        if(Input.GetKey(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.Space) && _characterController.isGrounded)
+            _fallVelocity = -jumpForce;
+        if (Input.GetKey(KeyCode.W))
             _moveVector += transform.forward;
-        if(Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D))
             _moveVector += transform.right;
-        if(Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S))
             _moveVector -= transform.forward;
-        if(Input.GetKey(KeyCode.A))
-            _moveVector -= transform.right;      
+        if (Input.GetKey(KeyCode.A))
+            _moveVector -= transform.right;
     }
+
     private void OnTriggerEnter(Collider other)
     {
-       if(this.CompareTag("Player") && other.CompareTag("Comp"))
-       {
-        can.active = false;
-        comp.active = true;
-        Cursor.lockState = CursorLockMode.Confined;
-        Cursor.visible = true;      
-       }        
+        if (this.CompareTag("Player") && other.CompareTag("Comp"))
+        {
+            can.active = false;
+            comp.active = true;
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+        }
     }
 
     public void ButtonComp()
     {
-     can.active = true;
-     comp.active = false;
-     Cursor.lockState = CursorLockMode.Confined;
-     Cursor.visible = false;         
+        can.active = true;
+        comp.active = false;
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = false;
     }
 
     public void ButtonLoad()
     {
-      load.fillAmount = load.fillAmount + 0.1f;
-      Destroy(Trigger);
+        load.fillAmount = load.fillAmount + 0.1f;
+        Destroy(Trigger);
 
     }
 
