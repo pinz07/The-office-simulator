a92ad9dfd16326143e7afd2a50609171676b85e5
DARKSAYONE <skyhard2017@yandex.ru>
2024-05-07 19:37:04 +0300

    Fixed quest

---
+++ Assets/Scripts/Quests/QuestEatFood.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestsEatFood : MonoBehaviour
+{
+    /*This is a test for one quest logic */
+    [Header("Quest Core")]
+    [SerializeField] private QuestsMain QuestsM;
+    [SerializeField] private bool ThisQuestComplete = false;
+    [SerializeField] private bool RewardGet = false;
+    [SerializeField] private PlayerStat PS;
+    [Header("Quest")]
+    [SerializeField] private GameObject Sandwich;
+    [SerializeField] private GameObject Water;
+    [SerializeField] bool SandwichEated = false;
+    [SerializeField] bool WaterDrinked = false;
+    void Start()
+    {
+        PS = GetComponentInParent<PlayerStat>();
+        if (PS == null)
+            Debug.LogError("PlayerStat not found");
+    }
+
+
+    void Update()
+    {
+        EatSandwich();
+        DrinkWater();
+
+        if(SandwichEated && WaterDrinked)
+        {
+            ThisQuestComplete = true;
+            GetRewardForQuest();
+            PS.PlayerDo = false;
+            Destroy(gameObject);
+        }
+    }
+
+    private void EatSandwich()
+    {
+        if(Input.GetMouseButton(1))
+        {
+            Debug.Log("Am am am");
+            Destroy(Sandwich);
+            SandwichEated =true;
+        }
+    }
+
+    private void DrinkWater()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("Glp glp glp");
+            Destroy(Water);
+            WaterDrinked = true;
+        }
+    }
+
+    private void GetRewardForQuest()
+    {
+        if (ThisQuestComplete && !RewardGet)
+        {
+            QuestsM.CurrentQuestsComplete++;
+            RewardGet = true;
+        }
+    }
+}
