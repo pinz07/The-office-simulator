50140783bd8ac538a3c1b69a74513d3b0800cc54
DARKSAYONE <skyhard2017@yandex.ru>
2024-05-08 20:57:53 +0300

    Lift

---
+++ Assets/Scripts/Quests/LiftDoors.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class LiftDoors : MonoBehaviour
+{
+    [SerializeField] private Transform FirstDoor;
+    [SerializeField] private Transform SecondDoor;
+    void Start()
+    {
+        DoorOpens();
+    }
+
+    private void DoorOpens()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
