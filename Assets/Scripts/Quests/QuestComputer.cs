e96602ea863667a5ba2f97823831632efd8aec3d
DARKSAYONE <skyhard2017@yandex.ru>
2024-05-08 16:32:29 +0300

    Add preform for worked PC minigame

---
+++ Assets/Scripts/Quests/QuestComputer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestsComputer : MonoBehaviour
+{
+    /*This is a test for one quest logic */
+    [Header("Quest Core")]
+    [SerializeField] private QuestsMain QuestsM;
+    [SerializeField] private bool RewardGet = false;
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+}
