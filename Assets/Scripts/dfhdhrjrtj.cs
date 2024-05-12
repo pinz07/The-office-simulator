f2f01085d73a9fdc8823587c41cc6e4954a18c1d
pinz07 <158826394+pinz07@users.noreply.github.com>
2024-05-12 17:29:37 +0300

    day_3

---
+++ Assets/Scripts/dfhdhrjrtj.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+
+public class Lightbulb : MonoBehaviour
+{
+
+    public float constantIntens;
+    public float inten;
+    private float TimeDown;
+
+    // Use this for initialization
+    void Start()
+    {
+        TimeDown = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (gameObject.GetComponent<Light>().intensity != constantIntens) gameObject.GetComponent<Light>().intensity = constantIntens;
+
+        if (TimeDown > 0) TimeDown -= Time.deltaTime;
+
+        if (TimeDown < 0) TimeDown = 0;
+
+        if (TimeDown == 0)
+        {
+            inten = Random.Range(0.2f, 4.0f);
+            gameObject.GetComponent<Light>().intensity = inten;
+            TimeDown = Random.Range(0.2f, 0.6f);
+        }
+    }
+}
